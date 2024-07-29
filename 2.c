fad826716bfd10bb633cc25ec2ad13266e2891fc
Aditya Raj <130920748+adityaraj0908@users.noreply.github.com>
2024-07-30 00:38:40 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,59 +1,72 @@
-#include <stdio.h> 
-#include <string.h> 
-int main() { 
-char filename[10],pattern[10],temp[200],res[100]; 
-FILE *fp; 
-printf("\n Enter file name:");  
-scanf("%s",filename); 
-printf("\n Enter the pattern:"); 
-scanf("%s",pattern); 
-fp=fopen(filename,"r");  
-if(fp==NULL) 
-{ 
-  printf("\n Error opening file.\n"); 
-  return 1;
-} 
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-while(!feof(fp)) 
-{ 
-  fgets(temp,sizeof(temp),fp); 
-  if(strstr(temp,pattern)) 
-
+void merge(int arr[],int l,int m,int r)
 {
-  printf("%s",temp); 
-} 
-}  
- 
-fclose(fp); 
-return 0; 
-} 
-//ls 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <dirent.h> 
-int main(int argc, char *argv[]) { 
-DIR *dir; 
-struct dirent *entry; 
-// Open the directory specified in the command-line argument or the current directory 
-if (argc == 1) { 
-dir = opendir("."); 
-} else if (argc == 2) { 
-dir = opendir(argv[1]); 
-} else { 
-printf("Usage: %s [directory]\n", argv[0]); 
-return 1; 
-} 
-// Check if directory was opened successfully 
-if (dir == NULL) {      
-return 1; 
-} 
-  perror("Error"); 
-// List directory contents 
-printf("Contents of directory:\n"); 
-while ((entry = readdir(dir )) != NULL) { 
-printf("%s\n", entry->d_name); 
-} 
-// Close the directory 
-closedir(dir); 
-return 0; 
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+    for(i=0;i<n1;i++)
+    {
+        L[i]=arr[i+l];
+    }
+    for(j=0;j<n2;j++)
+    {
+        R[j]=arr[m+1+j];
+    }
+    i=0;j=0;k=l;
+    while(i<n1 && j<n2)
+    {
+        if(L[i]<=R[j])
+        {   
+            arr[k++]=L[i++];
+        }
+        else
+        {
+            arr[k++]=R[j++];
+        }
+    }
+    while(i<n1)
+    {
+        arr[k++]=L[i++];
+    }
+    while(j<n2)
+    {
+        arr[k++]=R[j++];
+    }
+}
+void merge_sort(int arr[],int l,int r)
+{
+    if(l<r)
+    {
+        int m=l+(r-l)/2;
+        merge_sort(arr,l,m);
+        merge_sort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+}
+int main()
+{
+    int i,n,arr[100];
+    float start,end;
+    printf("Enter the number of elements\n");
+    scanf("%d",&n);
+    printf("\nArray before sorting: \n");
+    for(i=0;i<n;i++)
+    {
+        arr[i]=rand();
+        printf("%d  ",arr[i]);
+    }
+    start = clock();
+    merge_sort(arr,0,n-1);
+    end = clock();
+    printf("\nSorted array: \n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+    printf("\nTime taken in sorting is %f\n",(end-start)/CLOCKS_PER_SEC);
+
 }
